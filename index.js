fc80b2c5ba6a13e77785d7799ab94265c29f3d60
zlh <zawlinhtut74551@gmail.com>
2025-05-05 13:06:23 +0630

    Update input validation by using 'Promis' and 'async & await'

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// Button and Input
-const input = document.querySelectorAll(".input");
+// Button
 const button = document.querySelector(".button");
 // Eye Icon and Password
 const eyeIcon = document.querySelector(".eye");
-const password = document.getElementById("password");
 const passwordEyeIconErrorIcon = document.querySelector(
   ".password-eyeIcon-errorIcon"
 );
+// Input
+const inputFirstName = document.getElementById("fname");
+const inputLastName = document.getElementById("lname");
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
 // Error Content
 const errorContentFname = document.getElementById("errorContentFname");
 const errorContentLname = document.getElementById("errorContentLname");
@@ -18,114 +21,131 @@ const errorIconLname = document.getElementById("errorIconLname");
 const errorIconEmail = document.getElementById("errorIconEmail");
 const errorIconPassword = document.getElementById("errorIconPassword");
 
-button.addEventListener("click", () => {
-  input.forEach((input) => {
-    const inputPlaceholder = input.placeholder;
-    const inputValue = input.value;
+button.addEventListener("click", async () => {
+  // Check First Name
+  try {
+    const fname = await checkFirstName(inputFirstName.value.trim());
+    whenNoError(inputFirstName, errorContentFname, errorIconFname);
+    console.log(fname);
+  } catch (error) {
+    whenError(inputFirstName, errorContentFname, errorIconFname, error);
+  }
 
-    // Checking Input
-    // First Name Checking
-    if (inputPlaceholder === "First Name") {
-      if (!/[a-z\d]/gi.test(inputValue)) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentFname.textContent = "First Name cannot be empty";
-        errorIconFname.style.display = "block";
-      } else if (/[\d]/g.test(inputValue)) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentFname.textContent = "First Name cannot contain numbers";
-        errorIconFname.style.display = "block";
-      } else {
-        input.style.border = "1px solid hsl(246, 25%, 77%)";
-        input.style.boxShadow = "0 3px hsl(246, 25%, 77%)";
-        errorContentFname.textContent = "";
-        errorIconFname.style.display = "none";
-      }
-    }
+  // Check Last Name
+  try {
+    const lname = await checkLastName(inputLastName.value.trim());
+    whenNoError(inputLastName, errorContentLname, errorIconLname);
+    console.log(lname);
+  } catch (error) {
+    whenError(inputLastName, errorContentLname, errorIconLname, error);
+  }
 
-    // Last Name Checking
-    if (inputPlaceholder === "Last Name") {
-      if (!/[a-z\d]/gi.test(inputValue)) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentLname.textContent = "Last Name cannot be empty";
-        errorIconLname.style.display = "block";
-      } else if (/[\d]/g.test(inputValue)) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentLname.textContent = "Last Name cannot contain numbers";
-        errorIconLname.style.display = "block";
-      } else {
-        input.style.border = "1px solid hsl(246, 25%, 77%)";
-        input.style.boxShadow = "0 3px hsl(246, 25%, 77%)";
-        errorContentLname.textContent = "";
-        errorIconLname.style.display = "none";
-      }
-    }
+  // Check Email
+  try {
+    const email = await checkEmail(inputEmail.value.trim());
+    whenNoError(inputEmail, errorContentEmail, errorIconEmail);
+    console.log(email);
+  } catch (error) {
+    whenError(inputEmail, errorContentEmail, errorIconEmail, error);
+  }
 
-    // Email Checking
-    if (inputPlaceholder === "Email") {
-      if (!/[a-z\d]/gi.test(inputValue)) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentEmail.textContent = "Email cannot be empty";
-        errorIconEmail.style.display = "block";
-      } else if (
-        !/@gmail.com$/.test(inputValue) &&
-        /[a-z\d]/.test(inputValue)
-      ) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentEmail.textContent = "Looks like this is not an email";
-        errorIconEmail.style.display = "block";
-      } else {
-        input.style.border = "1px solid hsl(246, 25%, 77%)";
-        input.style.boxShadow = "0 3px hsl(246, 25%, 77%)";
-        errorContentEmail.textContent = "";
-        errorIconEmail.style.display = "none";
-      }
-    }
+  // Check Password
+  try {
+    const password = await checkPassword(inputPassword.value.trim());
+    whenNoError(inputPassword, errorContentPassword, errorIconPassword);
+    passwordEyeIconErrorIcon.style.transform = "translate(0, -50%)";
+    console.log(password);
+  } catch (error) {
+    whenError(inputPassword, errorContentPassword, errorIconPassword, error);
+    passwordEyeIconErrorIcon.style.transform = "translate(0, -80%)";
+  }
+});
 
-    // Password Checking
-    if (inputPlaceholder === "Password") {
-      if (!/[a-z\d]/gi.test(inputValue)) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentPassword.textContent = "Password cannot be empty";
-        errorIconPassword.style.display = "block";
-        passwordEyeIconErrorIcon.style.transform = "translate(0, -80%)";
-      } else if (!/[a-z\d]{4,}/gi.test(inputValue)) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentPassword.textContent = "Password must at least 4";
-        errorIconPassword.style.display = "block";
-        passwordEyeIconErrorIcon.style.transform = "translate(0, -80%)";
-      } else if (!/[a-z]/gi.test(inputValue) || !/\d/.test(inputValue)) {
-        input.style.border = "2px solid red";
-        input.style.boxShadow = "0 3px red";
-        errorContentPassword.textContent =
-          "Password must contain letters and numbers";
-        errorIconPassword.style.display = "block";
-        passwordEyeIconErrorIcon.style.transform = "translate(0, -80%)";
-      } else {
-        input.style.border = "1px solid hsl(246, 25%, 77%)";
-        input.style.boxShadow = "0 3px hsl(246, 25%, 77%)";
-        errorContentPassword.textContent = "";
-        errorIconPassword.style.display = "none";
-        passwordEyeIconErrorIcon.style.transform = "translate(0, -50%)";
-      }
+// When No Error Occurs
+function whenNoError(input, errorContent, errorIcon) {
+  console.log(input);
+  input.style.border = "1px solid hsl(246, 25%, 77%)";
+  input.style.boxShadow = "0 3px hsl(246, 25%, 77%)";
+  errorContent.textContent = "";
+  errorIcon.style.display = "none";
+}
+
+// When Error Occurs
+function whenError(input, errorContent, errorIcon, message) {
+  input.style.border = "2px solid red";
+  input.style.boxShadow = "0 3px red";
+  errorContent.textContent = message;
+  errorIcon.style.display = "block";
+}
+
+// First Name Checking Function
+function checkFirstName(fname) {
+  return new Promise((resolve, reject) => {
+    if (!/[a-z]/gi.test(fname)) {
+      reject("First Name cannot be empty!");
+    } else if (!/[a-z\d]{4,}/gi.test(fname)) {
+      reject("First Name must contain at least 4!");
+    } else if (/\d/g.test(fname)) {
+      reject("First Name cannot contain numbers!");
+    } else {
+      resolve(fname);
     }
   });
-});
+}
+
+// Last Name Checking Function
+function checkLastName(lname) {
+  return new Promise((resolve, reject) => {
+    if (!/[a-z]/gi.test(lname)) {
+      reject("Last Name cannot be empty!");
+    } else if (!/[a-z\d]{4,}/gi.test(lname)) {
+      reject("Last Name must contain at least 4!");
+    } else if (/\d/g.test(lname)) {
+      reject("Last Name cannot contain numbers!");
+    } else {
+      resolve(lname);
+    }
+  });
+}
+
+// Email Checking Function
+function checkEmail(email) {
+  return new Promise((resolve, reject) => {
+    if (!/[a-z\d]/gi.test(email)) {
+      reject("Email cannot be empty!");
+    } else if (!/^[a-zA-Z0-9._%+-]+@(gmail|outlook|yahoo)\.com$/.test(email)) {
+      reject("Looks like this is not an valid email!");
+    } else {
+      resolve(email);
+    }
+  });
+}
+
+// Password Checking Function
+function checkPassword(password) {
+  return new Promise((resolve, reject) => {
+    if (!/[a-z\d]/gi.test(password)) {
+      reject("Password cannot be empty!");
+    } else if (!/[\w]{6,}/gi.test(password) && /[a-z\d]/gi.test(password)) {
+      reject("Password must be at least 6!");
+    } else if (
+      !/^(?=(?:.*[a-zA-Z]){4,})(?=(?:.*\d){2,})/.test(password) &&
+      /[a-z\d]/gi.test(password)
+    ) {
+      reject("password must contain at least 4 letters and 2 numbers!");
+    } else {
+      resolve(password);
+    }
+  });
+}
 
 // Password Show or Hide
 eyeIcon.addEventListener("click", () => {
   if (eyeIcon.src.endsWith("eye-open.svg")) {
-    password.type = "text";
+    inputPassword.type = "text";
     eyeIcon.src = "images/eye-close.svg";
   } else {
-    password.type = "password";
+    inputPassword.type = "password";
     eyeIcon.src = "images/eye-open.svg";
   }
 });
